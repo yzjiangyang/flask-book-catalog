e73f4de7a19fa55fe9ce2e62f3ff1b9040461900
Yang Jiang <yzjiangyang123@163.com>
2021-07-06 14:48:11 -0700

    local

--- run.py
+++ run.py
@@ -1,14 +1,11 @@
 
 from app import create_app, db  # app/__init__ (ROOT PACKAGE)
 from app.auth.models import User
-from sqlalchemy import exc
 
-
-flask_app = create_app('prod')
-with flask_app.app_context():
-    db.create_all()
-    try:
+if __name__ == '__main__':
+    flask_app = create_app('dev')
+    with flask_app.app_context():
+        db.create_all()
         if not User.query.filter_by(user_name='harry').first():
             User.create_user(user='harry', email='harry@hogwarts.com', password='secret')
-    except exc.InternalError:
-        flask_app.run()
+    flask_app.run()
